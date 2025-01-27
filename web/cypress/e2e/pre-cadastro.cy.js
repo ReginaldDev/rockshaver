27fd904f474d5e6fe6eab1f140a783c01641f750
Reginaldo Silva de Albuquerque Junior <reginaldo.devtech@gmail.com>
2025-01-27 10:10:44 -0300

    Custom commands

--- web/cypress/e2e/pre-cadastro.cy.js
+++ web/cypress/e2e/pre-cadastro.cy.js
@@ -1,31 +1,26 @@
-import preRegPage from '../support/pages/pre-reg.page'
-import homePage from '../support/pages/home.page'
-import preReg from '../support/actions/prereg'
-import header from '../support/pages/components/header'
-
 
 describe('Pré-Cadastro', () => {
   it('Deve realizar o pré-cadastro do cliente', () => {
-    preReg.start('Bruno Teste', 'brunoteste@gmail.com')
-    preReg.verify('Bruno', 'brunoteste@gmail.com')
+    cy.startPreRegistration('Bruno Teste', 'brunoteste@gmail.com')
+    cy.verifyPreRegistered('Bruno', 'brunoteste@gmail.com')
   })
 
   it('Campos obrigatórios', () => {
-    preReg.start(null, null)
-    preReg.alert('Nome Completo', 'O campo nome é obrigatório.')
-    preReg.alert('E-mail', 'O campo e-mail é obrigatório.')
+    cy.startPreRegistration()
+    cy.alertHave('Nome Completo', 'O campo nome é obrigatório.')
+    cy.alertHave('E-mail', 'O campo e-mail é obrigatório.')
     
   })
 
   it('Não deve fazer o cadastro com o nome incompleto', () => {
-    preReg.start('Bruno', 'brunoteste@gmail.com')
-    preReg.alert('Nome Completo', 'Informe seu nome completo.')
+    cy.startPreRegistration('Bruno', 'brunoteste@gmail.com')
+    cy.alertHave('Nome Completo', 'Informe seu nome completo.')
     
   })
 
   it('Não deve fazer o cadastro com o e-mail incorreto', () => {
-    preReg.start('Bruno Teste', 'www.brunoteste.com')
-    preReg.alert('E-mail', 'O e-mail inserido é inválido.')
+    cy.startPreRegistration('Bruno Teste', 'www.brunoteste.com')
+    cy.alertHave('E-mail', 'O e-mail inserido é inválido.')
     
   })
 })
