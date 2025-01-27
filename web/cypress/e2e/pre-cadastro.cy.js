12d44c671b26f137faba466251afe0d9fd136fff
Reginaldo Silva de Albuquerque Junior <reginaldo.devtech@gmail.com>
2025-01-27 10:11:56 -0300

    Custom commands

--- web/cypress/e2e/pre-cadastro.cy.js
+++ web/cypress/e2e/pre-cadastro.cy.js
@@ -9,18 +9,15 @@ describe('Pré-Cadastro', () => {
     cy.startPreRegistration()
     cy.alertHave('Nome Completo', 'O campo nome é obrigatório.')
     cy.alertHave('E-mail', 'O campo e-mail é obrigatório.')
-    
   })
 
   it('Não deve fazer o cadastro com o nome incompleto', () => {
     cy.startPreRegistration('Bruno', 'brunoteste@gmail.com')
     cy.alertHave('Nome Completo', 'Informe seu nome completo.')
-    
   })
 
   it('Não deve fazer o cadastro com o e-mail incorreto', () => {
     cy.startPreRegistration('Bruno Teste', 'www.brunoteste.com')
     cy.alertHave('E-mail', 'O e-mail inserido é inválido.')
-    
   })
 })
