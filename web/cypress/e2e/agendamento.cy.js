5ad72a4269825bfafbbe52c61fef0a41fbdc1974
Reginaldo Silva de Albuquerque Junior <reginaldo.devtech@gmail.com>
2025-02-11 10:37:07 -0300

    new functions

--- web/cypress/e2e/agendamento.cy.js
+++ web/cypress/e2e/agendamento.cy.js
@@ -20,7 +20,7 @@ describe('Agendamento', () => {
 
 
         cy.iniciarAgendamento()
-        cy.escolherProfissional(agendamento.profissional)
+        cy.escolherProfissional(agendamento.profissional.nome)
         cy.selecionarServico(agendamento.servico.descricao)
         cy.escolherDiaAgendamento(agendamento.dia)
         cy.escolherHorarioAgendamento(agendamento.hora)
@@ -29,5 +29,36 @@ describe('Agendamento', () => {
         cy.get('h3')
             .should('be.visible')
             .and('have.text', 'Tudo certo por aqui! Seu horário está confirmado.')
+    })
+
+    it('Deve mostrar o slot ocupado', () => {
+        const agendamento = agendamentos.duplicado
+
+        cy.dropCollection('agendamentos', { failSilently: 'true' })
+            .then(result => {
+                cy.log(result);
+            });
+
+        cy.agendamentoApi(agendamento)
+
+        cy.intercept('GET', 'http://localhost:3333/api/calendario', {
+            statusCode: 200,
+            body: calendario
+        }).as('getCalendario')
+
+        cy.startPreRegistration(agendamento.usuario)
+        cy.verifyPreRegistered(agendamento.usuario)
+
+
+        cy.iniciarAgendamento()
+        cy.escolherProfissional(agendamento.profissional.nome)
+        cy.selecionarServico(agendamento.servico.descricao)
+        cy.escolherDiaAgendamento(agendamento.dia)
+        
+        cy.get(`[slot="${agendamento.hora} - ocupado"]`)
+            .should('be.visible')
+            .find('svg')
+            .should('be.visible')
+            .and('have.css', 'color', 'rgb(255, 255, 255)')
     })
 })
